004131103a5103ed7ccd1ab9a523d551ad453d0b
Abdhi <rizaabdhi@gmail.com>
2019-07-31 22:53:54 +0700

    clean code

--- app/src/main/java/com/abdhilabs/dasboarditckotlin/MainActivity.kt
+++ app/src/main/java/com/abdhilabs/dasboarditckotlin/MainActivity.kt
@@ -3,39 +3,26 @@ package com.abdhilabs.dasboarditckotlin
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Button
-import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var nama: EditText
-    private lateinit var user: EditText
-    private lateinit var email: EditText
-    private lateinit var asal: EditText
-    private lateinit var simpan: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        nama = findViewById(R.id.eNama)
-        user = findViewById(R.id.eUser)
-        email = findViewById(R.id.eEmail)
-        asal = findViewById(R.id.eAsal)
+        btnSimpan.setOnClickListener {
+            val strNama = eNama.text.toString()
+            val strUser = eUser.text.toString()
+            val strEmail = eEmail.text.toString()
+            val strAsal = eAsal.text.toString()
 
-        simpan = findViewById(R.id.btnSimpan)
-        simpan.setOnClickListener {
-            val strNama = nama.text.toString()
-            val strUser = user.text.toString()
-            val strEmail = email.text.toString()
-            val strAsal = asal.text.toString()
-
-            val btnSimpan = Intent(this, DashboardActivity::class.java)
-            btnSimpan.putExtra("NAMA", strNama)
-            btnSimpan.putExtra("USER", strUser)
-            btnSimpan.putExtra("EMAIL", strEmail)
-            btnSimpan.putExtra("ASAL", strAsal)
-            startActivity(btnSimpan)
+            val intent = Intent(this, DashboardActivity::class.java)
+            intent.putExtra("NAMA", strNama)
+            intent.putExtra("USER", strUser)
+            intent.putExtra("EMAIL", strEmail)
+            intent.putExtra("ASAL", strAsal)
+            startActivity(intent)
         }
     }
 }
